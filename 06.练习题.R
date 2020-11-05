49e30122591e71354e85b39c38956766e3029d00
yuxi-xi <73469487+yuxi-xi@users.noreply.github.com>
2020-11-05 23:54:54 +0800

    Add files via upload

---
+++ 06.练习题.R
@@ -0,0 +1,17 @@
+#6-1
+multiples_of_pi<-new.env()
+multiples_of_pi
+multiples_of_pi[["two_pi"]]<-2*pi
+multiples_of_pi$three_pi<-polyroot(3*pi)
+assign(
+  "four_pi",
+  4*pi,
+  multiples_of_pi
+)
+multiples_of_pi
+
+#6-2
+#i am sorry
+
+#6-3
+#i am more than sorry
